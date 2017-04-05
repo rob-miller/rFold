68e19482e7551da197e891ad12d7e30438d1a6b8
Rob Miller <rob@janerob.com>
2017-04-05 12:54:07 +0300

    update copyright message

--- build/buildProtein.lua
+++ build/buildProtein.lua
@@ -3,7 +3,7 @@
 --[[
    buildProtein.lua
    
-Copyright 2016 Robert T. Miller
+Copyright 2016, 2017 Robert T. Miller
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use these files except in compliance with the License.
