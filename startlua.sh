cbaf32004748955c1aee86b44984d7308239ce37
Rob Miller <rob@janerob.com>
2019-02-17 12:41:12 +0300

    remove notes files from repository

--- startlua.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LUA_PATH='/usr/local/lib/luarocks/rocks/?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;/home/rob/proj/rFold/lualib/?.lua'
-export LUA_CPATH='/usr/local/lib/lua/5.1/?.so;/usr/local/lib/luarocks/rocks/?.so;/home/rob/proj/rFold/lualib/?.so'
-
